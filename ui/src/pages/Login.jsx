6964f4c8f6342d6c5cea516dcaea1675c10afa40
Andrey Dolya <dolya7kk@gmail.com>
2024-08-03 20:00:10 +0300

    add tailwind and login basic layout

--- ui/src/pages/Login.jsx
+++ ui/src/pages/Login.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-layout border border-red-500 h-full">
+      <div className="login-box flex flex-col w-96">
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button className="">login</button>
+      </div>
+    </div>
+  );
+};
