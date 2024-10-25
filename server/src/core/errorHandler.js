d38e7ab853ae38548a5f896616de25b3acffb929
Andrey Dolya <dolya7kk@gmail.com>
2024-10-25 15:08:00 +0300

    remove

--- server/src/core/errorHandler.js
+++ server/src/core/errorHandler.js
@@ -10,14 +10,4 @@ export const setupErrorHandler = async (httpServer, logger, shutdown) => {
     logger.error(`Uncaught Exception thrown: ${error}`);
     shutdown(1);
   });
-
-  // process.on("SIGINT", () => {
-  //   logger.info("Received SIGINT. Shutting down gracefully...");
-  //   shutdown(0);
-  // });
-
-  // process.on("SIGTERM", () => {
-  //   logger.info("Received SIGTERM. Shutting down gracefully...");
-  //   shutdown(0);
-  // });
 };
