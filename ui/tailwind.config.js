9ef1aea70e6fc56ad69d2365350bfdeb48cf0898
Andrey Dolya <dolya7kk@gmail.com>
2024-11-15 13:22:28 +0200

    fix chart

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -15,6 +15,7 @@ export default {
       colors: {
         grayBg: "#F6F6F6",
         purpleBg: "rgb(116, 43, 225)", // sidebar card active
+        gradientBg: "HERE CHATGPT",
         purpleText: "#8A8FFF",
         greenish: "#00ff99", // logo color
         sidebarAndCardBg: "#171721",
