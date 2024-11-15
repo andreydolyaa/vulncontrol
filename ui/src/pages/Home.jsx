23ad12e8f048840e167fa26c7a08a46c932848ce
Andrey Dolya <dolya7kk@gmail.com>
2024-11-15 22:46:03 +0200

    add todos

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -5,31 +5,37 @@ import { Container } from "../components/Container/Container";
 import { Empty } from "../components/Empty";
 
 export const Home = () => {
+
+  // const style = {
+  //   ol {
+  //     list-style-type: decimal;
+  //   }
+  // }
   return (
-    <Container>
-      <pre>
+  
+      <div>
         TODO:
-        <ol>
-          <li>1) add confirmation modal on logout/delete of scans</li>
-          <li>2) add light theme</li>
-          <li>3) finish topbar</li>
-          <li>4) add manual toggle to sidebar</li>
-          <li>5) add Nikto</li>
-          <li>6) Overview - add recent scans</li>
-          <li>7) Overview - add auto start scan</li>
-          <li>8) add loading to modules in sidebar</li>
-          <li>9) add indication for debounce when searching</li>
-          <li>10) remove leftovers of tailwind and styled components and convert to css</li>
-          <li>11) server - add authorization middleware for jwt</li>
-          <li>12) fix login/register page styles and redirect from reg to login</li>
-          <li>13) add context menu to handle scans actions</li>
+        <ol style={{ listStyleType: "decimal" }}>
+          <li> add confirmation modal on logout/delete of scans</li>
+          <li> add light theme</li>
+          <li> finish topbar</li>
+          <li> add manual toggle to sidebar</li>
+          <li> add Nikto</li>
+          <li> Overview - add recent scans</li>
+          <li> Overview - add auto start scan</li>
+          <li> add loading to modules in sidebar</li>
+          <li> add indication for debounce when searching</li>
+          <li> remove leftovers of tailwind and styled components and convert to css</li>
+          <li> server - add authorization middleware for jwt</li>
+          <li> fix login/register page styles and redirect from reg to login</li>
+          <li> add context menu to handle scans actions</li>
 
           
           
           <li>*) rewrite with ts</li>
 
         </ol>
-      </pre>
-    </Container>
+      </div>
+
   );
 };
