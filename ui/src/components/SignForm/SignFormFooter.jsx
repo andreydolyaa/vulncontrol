64336b821cb34a5c8fe2a2567053083f7db527a4
Andrey Dolya <dolya7kk@gmail.com>
2024-11-20 22:47:55 +0200

    disable reg

--- ui/src/components/SignForm/SignFormFooter.jsx
+++ ui/src/components/SignForm/SignFormFooter.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Register } from "../../pages/Register";
 
 export const SignFormFooter = ({ isLoginPage }) => {
   return (
@@ -7,10 +8,13 @@ export const SignFormFooter = ({ isLoginPage }) => {
         <span className="text-gray-400 mx-3">OR</span>
       </div>
 
-      <div className="text-gray-400 text-center underline">
+      {/* <div className="text-gray-400 text-center underline">
         <a href={isLoginPage() ? "/register" : "/login"}>
           {isLoginPage() ? "Register New Account" : "Log In"}
         </a>
+      </div> */}
+      <div className="text-gray-600 text-center">
+        <p>Registration Temporarily unavailable</p>
       </div>
     </>
   );
