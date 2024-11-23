dc1aca89a2bb8ebafa454f8397ddfeb277577f90
Andrey Dolya <dolya7kk@gmail.com>
2024-11-23 22:00:09 +0200

    create geoip data in db

--- ui/src/pages/Map/customData.js
+++ ui/src/pages/Map/customData.js
@@ -1,7 +1,7 @@
 import L from "leaflet";
 
 export const customMarker = new L.Icon({
-  iconUrl: "/public/marker.svg",
+  iconUrl: "/marker.svg",
   iconSize: [24, 24],
   iconAnchor: [16, 24],
   popupAnchor: [0, -24],
